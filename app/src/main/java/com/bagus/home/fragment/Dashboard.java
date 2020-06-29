74bea7085bb6496d7daaa947be9feb20ed6ad22c
BagusSatria123 <satriabagusputra123@gmail.com>
2020-06-29 14:02:30 +0700

    Json Array List Country

--- app/src/main/java/com/bagus/home/fragment/Dashboard.java
+++ app/src/main/java/com/bagus/home/fragment/Dashboard.java
@@ -1,6 +1,7 @@
 package com.bagus.home.fragment;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,17 +10,39 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.bagus.home.R;
+
+import org.json.JSONArray;
 
 public class Dashboard extends Fragment {
 
     View view;
+    RequestQueue queue;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.dashboard,container,false);
 
+        queue = Volley.newRequestQueue(getActivity());
+        JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, "https://corona.lmao.ninja/v2/countries", null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("tampil",response + "");
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
         return view;
     }
 }
