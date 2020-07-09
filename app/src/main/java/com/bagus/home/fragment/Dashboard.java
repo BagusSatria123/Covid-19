5c2bc943554501c93442013697728a8ad1ccf5ae
BagusSatria123 <satriabagusputra123@gmail.com>
2020-07-09 07:36:47 +0700

    Dashboard reload negara bendera

--- app/src/main/java/com/bagus/home/fragment/Dashboard.java
+++ app/src/main/java/com/bagus/home/fragment/Dashboard.java
@@ -19,6 +19,8 @@ import com.android.volley.toolbox.Volley;
 import com.bagus.home.R;
 
 import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Dashboard extends Fragment {
 
@@ -34,8 +36,30 @@ public class Dashboard extends Fragment {
         JsonArrayRequest request = new JsonArrayRequest(Request.Method.GET, "https://corona.lmao.ninja/v2/countries", null, new Response.Listener<JSONArray>() {
             @Override
             public void onResponse(JSONArray response) {
-                Log.d("tampil",response + "");
+
+                for (int k = 0; k < response.length(); k++) {
+                    try {
+                        JSONObject obj = response.getJSONObject(k);
+
+                        String country = obj.getString("country");
+
+                        for (int i = 0; i < obj.length(); i++) {
+                            JSONObject obj1 = obj.getJSONObject("countryInfo");
+
+                            String flag = obj1.getString("flag");
+                            Log.d("flagku", flag + "");
+                        }
+
+                        Log.d("testing", country + "");
+//                        Log.d("tampil", response + "");
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+
+                    }
+                }
             }
+
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
